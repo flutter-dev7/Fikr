5ffa5c69370c213601e2dd29981d84713c8dcd0f
Ismoil <zajnalovismoil07@gmail.com>
2026-04-02 10:38:46 +0500

    Connect to Firebase Successfully

---
+++ android/app/src/main/kotlin/com/example/fikr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fikr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:fikr/core/themes/light_mode.dart';
+import 'package:fikr/firebase_options.dart';
+import 'package:fikr/pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+      theme: lightMode,
+    );
+  }
+}
